8bcb6fae46740e7fa05b1badf1ea2794cb033ca2
SamWhittington <69531345+SamWhittington@users.noreply.github.com>
2020-12-12 21:59:52 +0000

    Create replaceWithVowels.cpp
    Replace a string with vowels using a vector method

---
+++ replaceWithVowels.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+ 
+int main() {
+ 
+ std::string input = "Hello World You are a whale!";
+ std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+ std::vector<char> result;
+
+ for (int i = 0; i < input.size(); i++){
+   for (int j = 0; j < vowels.size(); j++) {
+     if (input[i] == vowels[j]){
+       result.push_back(input[i]);
+
+     }
+   }
+ }
+
+for (int k = 0; k < result.size(); k++) {
+ 
+  std::cout << result[k];
+ 
+}
+
+}
